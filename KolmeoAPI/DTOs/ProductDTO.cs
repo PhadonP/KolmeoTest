10a5727f033274c7dacc2d06eb53f990ec2a7754
Phadon Phipat <phadon.phipat@hydrix.com>
2022-11-10 11:54:10 +1100

    Add Model Validation for ProductDTO

--- KolmeoAPI/DTOs/ProductDTO.cs
+++ KolmeoAPI/DTOs/ProductDTO.cs
@@ -1,10 +1,14 @@
-﻿namespace KolmeoAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KolmeoAPI.DTOs
 {
     public record ProductDTO
     {
         public long Id { get; init; }
+        [Required]
         public string? Name { get; init; }
         public string? Description { get; init; }
+        [Range(0, double.MaxValue)]
         public decimal Price { get; init; }
     }
 }
